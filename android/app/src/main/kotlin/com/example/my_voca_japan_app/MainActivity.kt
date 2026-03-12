dc4314d9f302aa688d5ccc85f2b7b47183574ccf
Teachsue <a65854375@gmail.com>
2026-03-12 15:59:27 +0900

    Add_Final_Release

--- android/app/src/main/kotlin/com/example/my_voca_japan_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_voca_japan_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/nyang/voca/japan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nyang.voca.japan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
